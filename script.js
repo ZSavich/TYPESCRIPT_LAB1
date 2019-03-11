16bfcd1e88d06be777b60534531bc252d36b319b
zsavich <zsavich59@gmail.com>
2019-03-11 23:03:17 +0200

    Выполнил задание 1.2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+// Задание 1.1 Базовые типы
 function getAllWorkers() {
     return [
-        { name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000 },
-        { name: 'Petro', surname: 'Petrov', available: true, salary: 1500 },
-        { name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600 },
-        { name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300 }
+        { name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: 'Developer' },
+        { name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: 'Developer' },
+        { name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: 'QA' },
+        { name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: 'ScrumMaster' }
     ];
 }
 ;
 function logFirstAvailable(workers) {
-    if (workers === void 0) { workers = { name: string, surname: string, available: boolean, salary: number }; }
+    if (workers === void 0) { workers = { name: string, surname: string, available: boolean }; }
     console.log("\u0412 \u0441\u043F\u0438\u0441\u043A\u0435 " + workers.length + " \u0441\u043E\u0442\u0440\u0443\u0434\u043D\u0438\u043A\u0430");
     console.log("\u0414\u043E\u0441\u0442\u0443\u043F\u043D\u044B\u0435 \u0440\u0430\u0431\u043E\u0442\u043D\u0438\u043A\u0438:");
     for (var _i = 0, workers_1 = workers; _i < workers_1.length; _i++) {
@@ -17,4 +18,16 @@ function logFirstAvailable(workers) {
             console.log(worker.name + " " + worker.surname);
     }
 }
-logFirstAvailable(getAllWorkers());
+function getWorkersNamesByCategory(category) {
+    var workers = getAllWorkers();
+    var ourWorkers = [];
+    for (var _i = 0, workers_2 = workers; _i < workers_2.length; _i++) {
+        var worker = workers_2[_i];
+        worker.category === category ? ourWorkers.push(worker.surname) : '';
+    }
+    return ourWorkers;
+}
+function logWorkersNames(workers) {
+    console.log(workers.join(', '));
+}
+logWorkersNames(getWorkersNamesByCategory('Developer'));
--- script.ts
+++ script.ts
@@ -1,13 +1,15 @@
+// Задание 1.1 Базовые типы
+
 function getAllWorkers() {
     return [
-        {name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000},
-        {name: 'Petro', surname: 'Petrov', available: true, salary: 1500},
-        {name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600},
-        {name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300}
+        {name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: 'Developer'},
+        {name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: 'Developer'},
+        {name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: 'QA'},
+        {name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: 'ScrumMaster'}
     ];
 };
 
-function logFirstAvailable(workers = {name: string, surname: string, available: boolean, salary: number}): void {
+function logFirstAvailable(workers = {name: string, surname: string, available: boolean}): void {
     console.log(`В списке ${workers.length} сотрудника`);
     console.log(`Доступные работники:`);
     for (let worker of workers) {
@@ -16,4 +18,27 @@ function logFirstAvailable(workers = {name: string, surname: string, available: 
     }
 }
 
-logFirstAvailable(getAllWorkers());
+// Задание 1.2 Enum
+
+const enum Category {
+    BusinessAnalyst,
+    Developer,
+    Designer,
+    QA,
+    ScrumMaster
+}
+
+function getWorkersNamesByCategory(category: string) {
+    const workers = getAllWorkers();
+    let ourWorkers: Array<string> = [];
+    for (let worker of workers) {
+        worker.category === category ? ourWorkers.push(worker.surname) : '';
+    }
+    return ourWorkers;
+}
+
+function logWorkersNames(workers: string []): void {
+    console.log(workers.join(', '));
+}
+
+logWorkersNames(getWorkersNamesByCategory('Developer'));
